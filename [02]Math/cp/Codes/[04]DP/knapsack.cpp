e9331f3dd842d611ecf5551fd1fecc94dab2ceaa
Saom Bin Khaled <saombinkhaled3@gmail.com>
2024-09-17 00:34:32 +0600

    x

--- [02]Math/cp/Codes/[04]DP/knapsack.cpp
+++
@@ -1,8 +0,0 @@
-bool possible[W][N] = {false};
-possible[0][0] = true;
-	for (int k = 1; k <= n; k++) {
-	 for (int x = 0; x <= W; x++) {
-		if (x-w[k] >= 0) possible[x][k] |= possible[x-w[k]][k-1];
-		possible[x][k] |= possible[x][k-1];
-	}
-}
